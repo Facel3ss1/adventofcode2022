b2599d79657188717cd184ae562ed43acd701e41
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2022-12-01 08:48:08 +0000

    Day 1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let lines: Vec<Option<u32>> = include_str!("input.txt")
+        .lines()
+        .map(|l| l.parse().ok())
+        .collect();
+
+    let mut calorie_counts: Vec<u32> = lines
+        .split(Option::is_none)
+        .map(|ns| ns.iter().flatten().sum())
+        .collect();
+
+    calorie_counts.sort_unstable();
+    calorie_counts.reverse();
+
+    println!("Part 1: {}", calorie_counts[0]);
+    println!(
+        "Part 2: {}",
+        calorie_counts.into_iter().take(3).sum::<u32>()
+    );
 }
