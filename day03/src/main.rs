48e73a4ec34276814ae17c1c74f265ae89e4ec92
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2022-12-03 16:03:06 +0000

    Day 3 Part 2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,22 +1,52 @@
+use itertools::Itertools;
 use std::collections::HashSet;
+
+fn common_char<'a, G>(group: G) -> u8
+where
+    G: Iterator<Item = &'a [u8]>,
+{
+    group
+        .map(|items| items.iter().copied().collect::<HashSet<u8>>())
+        .reduce(|acc, items| &acc & &items)
+        .unwrap()
+        .into_iter()
+        .next()
+        .unwrap()
+}
+
+fn sum_groups<'a, I, G>(groups: I) -> u32
+where
+    G: Iterator<Item = &'a [u8]>,
+    I: Iterator<Item = G>,
+{
+    groups
+        .map(|group| {
+            let common_char = common_char(group);
+
+            if common_char.is_ascii_lowercase() {
+                // a-z -> 1-26
+                (common_char - 96) as u32
+            } else {
+                // A-Z -> 27-52
+                (common_char - 38) as u32
+            }
+        })
+        .sum()
+}
 
 fn main() {
     let lines = include_str!("input.txt").lines();
 
-    let mut sum = 0;
-    for line in lines {
-        let chars = line.bytes();
-        let first_half: HashSet<u8> = chars.clone().take(line.len() / 2).collect();
-        let second_half: HashSet<u8> = chars.skip(line.len() / 2).collect();
+    let part1_groups = lines.clone().map(|line| {
+        let chars = line.as_bytes();
+        let first_half = &chars[..line.len() / 2];
+        let second_half = &chars[line.len() / 2..];
 
-        let common_char = *first_half.intersection(&second_half).next().unwrap();
+        [first_half, second_half].into_iter()
+    });
 
-        if common_char.is_ascii_lowercase() {
-            sum += (common_char - 96) as u32;
-        } else {
-            sum += (common_char - 38) as u32;
-        }
-    }
+    let part2_groups = lines.map(str::as_bytes).chunks(3);
 
-    println!("Part 1: {sum}");
+    println!("Part 1: {}", sum_groups(part1_groups));
+    println!("Part 2: {}", sum_groups(part2_groups.into_iter()));
 }
