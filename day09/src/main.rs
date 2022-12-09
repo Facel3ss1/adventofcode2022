18be9075549c2a9ff9a575c36f7e151376575299
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2022-12-09 21:40:35 +0000

    Day 9 Part 2

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,17 +1,17 @@
 use std::collections::HashSet;
 
-fn main() {
-    let lines = include_str!("input.txt").lines();
+fn solve(input: &str, rope_length: usize) -> usize {
+    let lines = input.lines();
 
     let mut visited_positions: HashSet<(i32, i32)> = HashSet::new();
-    let mut head_position = (0, 0);
-    let mut tail_position = (0, 0);
-    visited_positions.insert(tail_position);
+    visited_positions.insert((0, 0));
+    let mut rope: Vec<(i32, i32)> = vec![(0, 0); rope_length];
 
     for line in lines {
         let (direction, steps) = line.split_once(' ').unwrap();
         let steps = steps.parse().unwrap();
 
+        let head_position = rope.last_mut().unwrap();
         match direction {
             "R" => head_position.0 += steps,
             "L" => head_position.0 -= steps,
@@ -21,19 +21,30 @@ fn main() {
         }
 
         for _ in 0..steps {
-            let vector = (
-                head_position.0 - tail_position.0,
-                head_position.1 - tail_position.1,
-            );
+            for i in (0..rope.len()).rev().skip(1) {
+                let prev_position = rope[i + 1];
+                let knot_position = &mut rope[i];
 
-            if vector.0.abs() > 1 || vector.1.abs() > 1 {
-                tail_position.0 += vector.0.signum();
-                tail_position.1 += vector.1.signum();
+                let vector = (
+                    prev_position.0 - knot_position.0,
+                    prev_position.1 - knot_position.1,
+                );
+
+                if vector.0.abs() > 1 || vector.1.abs() > 1 {
+                    knot_position.0 += vector.0.signum();
+                    knot_position.1 += vector.1.signum();
+                }
             }
 
-            visited_positions.insert(tail_position);
+            visited_positions.insert(rope[0]);
         }
     }
 
-    println!("Part 1: {}", visited_positions.len());
+    visited_positions.len()
+}
+
+fn main() {
+    let input = include_str!("input.txt");
+    println!("Part 1: {}", solve(input, 2));
+    println!("Part 2: {}", solve(input, 10));
 }
