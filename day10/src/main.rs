0df08bc67ae93d556f8a28b5122774d88a51c4eb
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2022-12-10 14:11:02 +0000

    Day 10 Part 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -4,41 +4,54 @@ struct Cpu {
 }
 
 fn main() {
-    let mut lines = include_str!("input.txt").lines();
     let mut cpu = Cpu {
         x: 1,
         cycle_count: 0,
     };
 
-    let signal_strengths_sum = std::iter::successors(Some(20), |n: &i32| n.checked_add(40))
+    let mut signal_strengths_sum = 0;
+    let mut cycle_numbers = std::iter::successors(Some(20), |n: &i32| n.checked_add(40))
         .take(6)
-        .map(|cycle_number| {
-            let mut signal_strength = None;
-            for line in lines.by_ref() {
-                let mut line = line.split_whitespace();
-                let instruction = line.next().unwrap();
-                let operand = line.next().map(|op| op.parse().unwrap()).unwrap_or(0);
+        .peekable();
 
-                match instruction {
-                    "noop" => cpu.cycle_count += 1,
-                    "addx" => cpu.cycle_count += 2,
-                    _ => panic!("Unsupported instruction"),
-                }
+    for line in include_str!("input.txt").lines() {
+        let mut line = line.split_whitespace();
+        let instruction = line.next().unwrap();
+        let operand = line.next().map(|op| op.parse().unwrap()).unwrap_or(0);
 
-                if cpu.cycle_count >= cycle_number {
-                    signal_strength = Some(cycle_number * cpu.x);
-                }
+        let cycle_count = match instruction {
+            "noop" => 1,
+            "addx" => 2,
+            _ => panic!("Unsupported instruction"),
+        };
 
-                cpu.x += operand;
+        for _ in 0..cycle_count {
+            cpu.cycle_count += 1;
 
-                if signal_strength.is_some() {
-                    break;
-                }
+            if let Some(cycle_number) = cycle_numbers
+                .peek()
+                .copied()
+                .filter(|&cycle_number| cpu.cycle_count == cycle_number)
+            {
+                signal_strengths_sum += cycle_number * cpu.x;
+                cycle_numbers.next();
             }
 
-            signal_strength.unwrap()
-        })
-        .sum::<i32>();
+            let position = (cpu.cycle_count - 1) % 40;
+            if (cpu.x - 1..=cpu.x + 1).contains(&position) {
+                print!("#");
+            } else {
+                print!(" ");
+            }
 
+            if cpu.cycle_count % 40 == 0 {
+                println!();
+            }
+        }
+
+        cpu.x += operand;
+    }
+
+    println!();
     println!("Part 1: {signal_strengths_sum}");
 }
