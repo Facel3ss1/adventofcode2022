bb6b2af564398d10aae75dd8f790da57c2a6ff30
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2022-12-11 14:47:01 +0000

    Day 11 Part 2

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,6 +1,6 @@
 mod parse;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Monkey {
     items: Vec<u64>,
     op: Op,
@@ -12,8 +12,10 @@ struct Monkey {
 }
 
 impl Monkey {
-    fn do_turn(&mut self) -> impl Iterator<Item = (usize, u64)> + '_ {
-        self.items.drain(..).map(|mut item| {
+    fn do_turn(&mut self, lcm: Option<u64>) -> Vec<(usize, u64)> {
+        let mut thrown_items = Vec::new();
+
+        for mut item in self.items.drain(..) {
             let rhs = match self.rhs {
                 Rhs::Old => item,
                 Rhs::Const(num) => num,
@@ -24,7 +26,10 @@ impl Monkey {
                 Op::Times => item *= rhs,
             }
 
-            item /= 3;
+            // Addition and multiplication work in modular arithmetic
+            if let Some(lcm) = lcm {
+                item %= lcm;
+            }
 
             let throw_to = if item % self.divisible_by == 0 {
                 self.throw_if_true
@@ -34,40 +39,62 @@ impl Monkey {
 
             self.num_inspected_items += 1;
 
-            (throw_to, item)
-        })
+            thrown_items.push((throw_to, item));
+        }
+
+        thrown_items
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Op {
     Add,
     Times,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Rhs {
     Old,
     Const(u64),
 }
 
+fn calc_monkey_business(mut monkeys: Vec<Monkey>) -> usize {
+    monkeys.sort_unstable_by_key(|monkey| monkey.num_inspected_items);
+    monkeys.reverse();
+    monkeys[0].num_inspected_items * monkeys[1].num_inspected_items
+}
+
 fn main() {
     let input = include_str!("input.txt").split("\n\n");
-    let mut monkeys: Vec<Monkey> = input.map(|monkey| monkey.parse().unwrap()).collect();
+    let mut monkeys_part1: Vec<Monkey> = input.map(|monkey| monkey.parse().unwrap()).collect();
+    let mut monkeys_part2 = monkeys_part1.clone();
 
     for _ in 0..20 {
-        for i in 0..monkeys.len() {
-            let monkey = &mut monkeys[i];
-            let thrown_items: Vec<(usize, u64)> = monkey.do_turn().collect();
+        for i in 0..monkeys_part1.len() {
+            let monkey = &mut monkeys_part1[i];
+            let thrown_items = monkey.do_turn(None);
             for (throw_to, item) in thrown_items {
-                monkeys[throw_to].items.push(item);
+                monkeys_part1[throw_to].items.push(item / 3);
             }
         }
     }
 
-    monkeys.sort_unstable_by_key(|monkey| monkey.num_inspected_items);
-    monkeys.reverse();
-    let monkey_business = monkeys[0].num_inspected_items * monkeys[1].num_inspected_items;
+    // All the divisors are different prime numbers, so the LCM is just the product of them
+    let lcm = monkeys_part2
+        .iter()
+        .map(|monkey| monkey.divisible_by)
+        .product();
 
-    println!("Part 1: {monkey_business}");
+    for _ in 0..10_000 {
+        for i in 0..monkeys_part2.len() {
+            let monkey = &mut monkeys_part2[i];
+            let thrown_items = monkey.do_turn(Some(lcm));
+            for (throw_to, item) in thrown_items {
+                monkeys_part2[throw_to].items.push(item);
+            }
+        }
+    }
+
+    println!("Part 1: {}", calc_monkey_business(monkeys_part1));
+    println!("Part 2: {}", calc_monkey_business(monkeys_part2));
 }
