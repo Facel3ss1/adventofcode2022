7c3d5628d2154971a48f9cc8356d40e5364ae997
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2022-12-16 00:09:52 +0000

    Tweak parsing in Day 13

--- day13/src/parse.rs
+++ day13/src/parse.rs
@@ -12,7 +12,7 @@ impl FromStr for Packet {
 
 fn parse_packet(input: &str) -> (Packet, &str) {
     match input.chars().next().unwrap() {
-        '[' => parse_list(&input[1..]),
+        '[' => parse_list(input),
         _ => parse_number(input),
     }
 }
@@ -20,6 +20,7 @@ fn parse_packet(input: &str) -> (Packet, &str) {
 fn parse_list(mut input: &str) -> (Packet, &str) {
     let mut list = Vec::new();
 
+    input = input.strip_prefix('[').unwrap();
     loop {
         match input.chars().next().unwrap() {
             ']' => break,
@@ -37,7 +38,7 @@ fn parse_list(mut input: &str) -> (Packet, &str) {
 
 fn parse_number(input: &str) -> (Packet, &str) {
     let (number, rest) = input
-        .find(|c| c == ',' || c == ']')
+        .find(|c: char| !c.is_ascii_digit())
         .map(|i| input.split_at(i))
         .unwrap_or((input, ""));
     (Packet::Number(number.parse().unwrap()), rest)
