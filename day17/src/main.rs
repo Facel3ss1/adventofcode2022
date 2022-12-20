634c5ba9f12b906c8be3c62dfa38b2f22df5692b
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2022-12-20 00:48:21 +0000

    Day 17 Part 2

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, VecDeque};
+
 const SHAPES: [[[bool; 4]; 4]; 5] = [
     [
         [false, false, false, false],
@@ -36,7 +38,7 @@ enum Direction {
     Right,
 }
 
-fn check_collision(shape: usize, position: (usize, usize), rows: &[[bool; 7]]) -> bool {
+fn check_collision(shape: usize, position: (usize, usize), rows: &VecDeque<[bool; 7]>) -> bool {
     let mut rightmost_col = 0;
     'outer: for col in (0..4).rev() {
         for row in 0..4 {
@@ -67,50 +69,36 @@ fn check_collision(shape: usize, position: (usize, usize), rows: &[[bool; 7]]) -
     did_collide
 }
 
-fn print_board(shape: usize, position: (usize, usize), rows: &[[bool; 7]]) {
-    for (row, cells) in rows.iter().enumerate().rev().take(30) {
-        for (col, b) in cells.iter().copied().enumerate() {
-            if b || position
-                .0
-                .checked_sub(row)
-                .zip(col.checked_sub(position.1))
-                .and_then(|(row, col)| SHAPES[shape].get(row).and_then(|cells| cells.get(col)))
-                .copied()
-                .filter(|&b| b)
-                .is_some()
-            {
-                print!("#");
-            } else {
-                print!(".");
-            }
+fn highest_row(rows: &VecDeque<[bool; 7]>) -> usize {
+    let mut highest_row = 0;
+    'outer: for row in (0..rows.len()).rev() {
+        if rows[row].iter().any(|&b| b) {
+            highest_row = row + 1;
+            break 'outer;
         }
-        println!();
     }
-    println!();
-    std::io::stdin().read_line(&mut String::new());
+
+    highest_row
 }
 
-fn simulate(total_shapes: usize) -> usize {
-    let mut directions = include_str!("input.txt")
-        .lines()
-        .flat_map(|line| {
-            line.chars().map(|c| match c {
-                '>' => Direction::Right,
-                '<' => Direction::Left,
-                _ => panic!("Not a direction"),
-            })
-        })
-        .cycle();
+fn simulate<I>(directions: I, total_shapes: usize) -> usize
+where
+    I: Iterator<Item = Direction> + Clone,
+{
+    let mut directions = directions.enumerate().cycle().peekable();
 
-    let mut rows: Vec<[bool; 7]> = vec![[false; 7]; 7];
+    let mut rows: VecDeque<[bool; 7]> = VecDeque::from([[false; 7]; 7]);
     let mut num_shapes = 0;
+    let mut tower_height = 0;
 
-    for shape in (0..5).cycle() {
+    let mut shape_history: HashMap<(usize, usize), (usize, usize)> = HashMap::new();
+
+    for shape in (0..SHAPES.len()).cycle() {
         let mut position: (usize, usize) = (rows.len() - 1, 2);
 
-        for direction in directions.by_ref() {
-            // print_board(shape, position, &rows);
+        let old_highest_row = highest_row(&rows);
 
+        for (_, direction) in directions.by_ref() {
             match direction {
                 Direction::Left => {
                     if let Some(new_col) = position.1.checked_sub(1) {
@@ -125,8 +113,6 @@ fn simulate(total_shapes: usize) -> usize {
                     }
                 }
             }
-
-            // print_board(shape, position, &rows);
 
             if check_collision(shape, (position.0 - 1, position.1), &rows) {
                 break;
@@ -143,34 +129,50 @@ fn simulate(total_shapes: usize) -> usize {
             }
         }
 
-        let mut highest_row = 0;
-        'outer: for row in (0..rows.len()).rev() {
-            if rows[row].iter().any(|&b| b) {
-                highest_row = row + 1;
-                break 'outer;
-            }
+        let new_highest_row = highest_row(&rows);
+        rows.extend(std::iter::repeat([false; 7]).take((new_highest_row + 7) - rows.len()));
+        while rows.len() > 1000 {
+            rows.pop_front();
         }
 
-        rows.extend(std::iter::repeat([false; 7]).take((highest_row + 7) - rows.len()));
-
         num_shapes += 1;
+        tower_height += new_highest_row - old_highest_row;
+
+        // Give it some time to settle into a repeating pattern
+        if num_shapes > 10000 {
+            if let Some((prev_num_shapes, prev_tower_height)) = shape_history.insert(
+                (shape, directions.peek().unwrap().0),
+                (num_shapes, tower_height),
+            ) {
+                let period = num_shapes - prev_num_shapes;
+                let shapes_left = total_shapes - num_shapes;
+                let num_periods = shapes_left / period;
+                num_shapes += num_periods * period;
+
+                let height_difference = tower_height - prev_tower_height;
+                tower_height += num_periods * height_difference;
+
+                shape_history.clear();
+            }
+        }
 
         if num_shapes == total_shapes {
             break;
         }
     }
 
-    let mut highest_row = 0;
-    'outer: for row in (0..rows.len()).rev() {
-        if rows[row].iter().any(|&b| b) {
-            highest_row = row + 1;
-            break 'outer;
-        }
-    }
-
-    highest_row
+    tower_height
 }
 
 fn main() {
-    println!("Part 1: {}", simulate(2022));
+    let directions = include_str!("example_input.txt").lines().flat_map(|line| {
+        line.chars().map(|c| match c {
+            '>' => Direction::Right,
+            '<' => Direction::Left,
+            _ => panic!("Not a direction"),
+        })
+    });
+
+    println!("Part 1: {}", simulate(directions.clone(), 2022));
+    println!("Part 2: {}", simulate(directions, 1_000_000_000_000));
 }
